9af4c003efdc62eadf9b60e79f9a10762cdbd101
soumya-94 <soumya11207@gmail.com>
2020-04-29 04:34:38 +0530

    modularised

--- Calculator Layout iOS13/Model/CalculatorBrain.swift
+++ Calculator Layout iOS13/Model/CalculatorBrain.swift
@@ -10,11 +10,130 @@ import Foundation
 
 struct CalculatorBrain {
     
-    struct Operation {
-        
-        
+    var equationToBeDisplayed = "0"
+    var equationToBeEvaluated = "0"
+    
+    mutating func resetEquationsToZero()
+    {
+        equationToBeDisplayed = "0"
+        equationToBeEvaluated = "0"
     }
     
-    //var operations: Dictionary<String, 
+    mutating func insertDigit(digit: String) -> String
+    {
+        if(equationToBeDisplayed == "0" && equationToBeEvaluated == "0")
+        {
+            equationToBeDisplayed = ""
+            equationToBeEvaluated = ""
+        }
+        equationToBeDisplayed = equationToBeDisplayed + digit
+        equationToBeEvaluated = equationToBeEvaluated + digit
+        return equationToBeDisplayed
+    }
+    
+    func checkSubstringExistence(in string: String, forAnyIn characters: String) -> Bool {
+        let customSet = CharacterSet(charactersIn: characters)
+        let inputSet = CharacterSet(charactersIn: string)
+        return !inputSet.intersection(customSet).isEmpty
+    }
+    
+    mutating func insertDecimal(currentDisplay: String) -> (toBeDisplayed: String, doubleCheck: Bool)
+    {
+        if(checkSubstringExistence(in: currentDisplay, forAnyIn: "+-×÷") == false && checkSubstringExistence(in: currentDisplay, forAnyIn: ".") == true)
+        { return (currentDisplay, false) }
+        else
+        {
+            equationToBeDisplayed = equationToBeDisplayed + "."
+            equationToBeEvaluated = equationToBeEvaluated + "."
+            return (equationToBeDisplayed, true)
+        }
+    }
+    
+    func prepareEquationForEval(equation: String) -> String
+    {
+        let equation1 = equation.replacingOccurrences(of: "×", with: "*")
+        let equation2 = equation1.replacingOccurrences(of: "÷", with: "/")
+        return equation2
+    }
+    
+    func evaluateEquation(equation: String) -> Double
+    {
+        let mathExpression = NSExpression(format: prepareEquationForEval(equation: equationToBeEvaluated))
+        let mathValue = mathExpression.expressionValue(with: nil, context: nil) as? Double
+        return mathValue!
+    }
+    
+    mutating func insertOperand(operand: String, decimalCheck: Bool) -> (toBeDisplayed: String, doubleCheck: Bool)
+    {
+        if(operand == "%")
+        {
+            if(equationToBeDisplayed == "0" && equationToBeEvaluated == "0")
+            { return (equationToBeDisplayed, false) }
+            else
+            {
+                let result = evaluateEquation(equation: equationToBeEvaluated)
+                let final = result/100
+                equationToBeDisplayed = String(final)
+                equationToBeEvaluated = String(final)
+                return (String(final), true)
+            }
+        }
+        else if (operand == "+/-")
+        {
+            let result = evaluateEquation(equation: equationToBeEvaluated)
+            let final = 0.0-(result)
+            if(decimalCheck == false && final.truncatingRemainder(dividingBy: 1)==0)
+            {
+                equationToBeDisplayed = String(Int(final))
+                equationToBeEvaluated = String(Int(final))
+                return (String(Int(final)), false)
+            }
+            else
+            {
+                equationToBeDisplayed = String(final)
+                equationToBeEvaluated = String(final)
+                return (String(final), false)
+            }
+        }
+        else
+        {
+            equationToBeDisplayed = equationToBeDisplayed + operand
+            if(decimalCheck == false)
+            {
+                equationToBeEvaluated = equationToBeEvaluated + ".0" + operand
+            }
+            else
+            {
+                equationToBeEvaluated = equationToBeEvaluated + operand
+            }
+            return (equationToBeDisplayed, false)
+        }
+    }
+    
+    mutating func calculate(currentDisplay: String, decimalCheck: Bool) -> (toBeDisplayed: String, doubleCheck: Bool)
+    {
+        if(checkSubstringExistence(in: currentDisplay, forAnyIn: "+-×÷") == false)
+        { return (currentDisplay, decimalCheck) }
+        else
+        {
+            if(decimalCheck == false)
+            {
+                equationToBeEvaluated = equationToBeEvaluated + ".0"
+            }
+            let final = evaluateEquation(equation: equationToBeEvaluated)
+            if(decimalCheck == false && final.truncatingRemainder(dividingBy: 1)==0)
+            {
+                equationToBeDisplayed = String(Int(final))
+                equationToBeEvaluated = String(final)
+                return (String(Int(final)), false)
+            }
+            else
+            {
+                equationToBeDisplayed = String(final)
+                equationToBeEvaluated = String(final)
+                return (String(final), false)
+            }
+        }
+    }
     
 }
